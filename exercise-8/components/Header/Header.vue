fc2ee1648605d65713b0fb4c45a77b0f4558ef6f
Andrej <andrej@loparev.it>
2021-01-29 20:59:41 +0100

    add menu

---
+++ exercise-8/components/Header/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container mx-auto p-5">
+    <nav class="flex-row md:justify-between">
+      <div class="flex flex-row justify-between">
+        <a href="#">Logo</a>
+
+        <button v-on:click="toggleNavbar" id="hamburgerbtn" class="md:hidden">
+          menu
+        </button>
+      </div>
+
+      <ul v-bind:class="{active: showMenu}" class="hidden md:flex md:flex-row" id="mobileMenu">
+        <li class="pr-5"><a> Services </a></li>
+        <li class="pr-5"><a>Porfolio</a></li>
+        <li class="pr-5"><a>About</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    toggleNavbar: function() {
+      this.showMenu = !this.showMenu;
+    }
+  }
+}
+</script>
+
+<style>
+  .active{
+    display: block;
+  }
+</style>
