098e9818f6521ba766de45c01745b60f64a2c108
Andrej <andrej@loparev.it>
2021-01-30 11:45:46 +0100

    pretty formatting

--- exercise-8/components/Header/Header.vue
+++ exercise-8/components/Header/Header.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="container mx-auto p-5">
     <nav class="flex-row md:justify-between">
-      <div class="flex flex-row justify-between">
-        <a href="#">Logo</a>
 
-        <button v-on:click="toggleNavbar" id="hamburgerbtn" class="md:hidden">
-          menu
-        </button>
+      <div class="flex flex-row justify-between">
+<!--        <a href="#">Logo</a>-->
+        <button v-on:click="toggleNavbar" id="hamburgerbtn" class="md:hidden">MENU</button>
       </div>
 
       <ul v-bind:class="{active: showMenu}" class="hidden md:flex md:flex-row" id="mobileMenu">
-        <li class="pr-5"><a> Services </a></li>
+        <li class="pr-5"><a>Services</a></li>
         <li class="pr-5"><a>Porfolio</a></li>
         <li class="pr-5"><a>About</a></li>
-        <li><a>Contact</a></li>
+        <li class="pr-5"><a>Contact</a></li>
       </ul>
+
     </nav>
   </div>
 </template>
